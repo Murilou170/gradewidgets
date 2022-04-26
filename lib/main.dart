a64e0af05e853a9eb89d30ea4313d3bc3a6e01a1
murilo <murilra@hotmail.com>
2022-04-26 15:50:27 -0300

    gradeviw com navegação nomeada

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,11 @@ import 'tela1.dart';
 
 void main() => runApp(MaterialApp(
       initialRoute: '/',
-      routes: {
-        '/': (context) => Home(),
-        '/tela1': (context) => tela1(),
-        '/tela2':(context) => tela2(),
-        '/tela3':(context) => tela3(),
-        '/tela4':(context) => tela4(),
-      },
+  routes: {
+    '/': (context) => Home(),
+    '/tela1': (context) => tela1(),
+    '/tela2': (context) => tela2(),
+    '/tela3': (context) => tela3(),
+    '/tela4': (context) => tela4(),
+  },
     ));
